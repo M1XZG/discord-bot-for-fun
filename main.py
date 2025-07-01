917d020ad12304c945af95b735a78eda84315fd8
Robert McKenzie <rmckenzi@rpmdp.com>
2025-07-01 12:21:58 +0100

    Fixing presence intents access

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.presences = True  # <-- Add this line
 
 bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
